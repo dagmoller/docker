6ed72a89494ef0ccb648874a17483697a81806f6
Diego Aguirre <aguirre@dagcs.com>
2022-09-06 11:20:08 -0300

    MyVD: Auto add jar files to myvd lib directory

--- myvd/entrypoint.sh
+++ myvd/entrypoint.sh
@@ -2,15 +2,21 @@
 
 basepath=/opt/myvd
 confpath=$basepath/conf
+libpath=$basepath/lib
 etcpath=/etc/myvd
 
 # Populate config path if file difers
 for item in $etcpath/*; do
 	name=$(basename $item)
+
 	if [ -e $confpath/$name ]; then
 		rm -rf $confpath/$name
 	fi
 	ln -s $item $confpath/$name
+
+	if [ $(echo $name | grep -ic '.jar') -gt 0 ]; then
+		ln -s $item $libpath/$name
+	fi
 done
 
 cd $basepath/bin
