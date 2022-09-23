4a5730383e44b507ed3a7de8e465380bb66a912d
Diego Aguirre <aguirre@dagcs.com>
2022-09-23 16:24:59 -0300

    Change permissions

--- freeradius/entry.sh
+++ freeradius/entry.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+echo 
+echo "Checking config path..."
+if [ ! -e /etc/raddb/radiusd.conf ]; then
+	echo "Populating /etc/raddb..."
+	cp -rf /etc/_raddb.default /etc/raddb
+fi
+echo "Adjusting config files permissions..."
 chmod 755 /etc/raddb
+
+echo "Starting radisud..."
 /usr/sbin/radiusd -f -l stdout
 
